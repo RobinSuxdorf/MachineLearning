97b66ffe8861c5100c9fe87615ed124cd0a2d8f7
Robin Suxdorf <robin.suxdorf@t-online.de>
2024-06-03 22:46:39 +0200

    Added doc strings

--- metrics/classification.py
+++ metrics/classification.py
@@ -2,10 +2,30 @@ from typing import Any, Optional
 import numpy as np
 
 def _check_length(list1: list[Any], list2: list[Any]) -> None:
+    """
+    Checks if the two lists have the same length.
+
+    Args:
+        list1 (list[Any]): The first list to check.
+        list2 (list[Any]): The second list to check.
+
+    Raises:
+        ValueError: If the lists do not have the same length.
+    """
     if len(list1) != len(list2):
         raise ValueError("The lists do not have the same length.")
 
 def _check_type(list1: list[Any], list2: list[Any]) -> None:
+    """
+    Checks if all elements in both lists are of the same type.
+
+    Args:
+        list1 (list[Any]): The first list to check.
+        lst2 (list[Any]): The second list to check.
+
+    Raises:
+        ValueError: If not all elements of list1 and list2 have the same type.
+    """
     if not list1:
         raise ValueError("The first list is empty.")
 
@@ -18,6 +38,16 @@ def _check_type(list1: list[Any], list2: list[Any]) -> None:
         raise ValueError("Not all elements in the second list are of the same type as the first element of the first list.")
 
 def accuracy_score(y_true: list[Any], y_pred: list[Any]) -> float:
+    """
+    Calculates the accuracy score, which is the proportion of correct predictions.
+
+    Args:
+        y_true (list[Any]): The list of true labels.
+        y_pred (list[Any]): The list of predicted labels.
+
+    Returns:
+        float: The accuracy score as float between 0 and 1.
+    """
     _check_length(y_true, y_pred)
     _check_type(y_true, y_pred)
 
@@ -25,7 +55,17 @@ def accuracy_score(y_true: list[Any], y_pred: list[Any]) -> float:
 
     return correct_predictions / len(y_true)
 
-def confusion_matrix(y_true: list[Any], y_pred: list[Any], class_labels: Optional[list[Any]] = None) -> np.array:
+def confusion_matrix(y_true: list[Any], y_pred: list[Any], class_labels: Optional[list[Any]] = None) -> np.ndarray:
+    """
+    Calculates the confusion matrix.
+
+    Args:
+        y_true (list[Any]): The list of true labels.
+        y_pred (list[Any]): The list of predicted labels.
+
+    Returns:
+        np.ndarray: The confusion matrix as numpy array of arrays.
+    """
     _check_length(y_true, y_pred)
     _check_type(y_true, y_pred)
 
@@ -47,7 +87,19 @@ def confusion_matrix(y_true: list[Any], y_pred: list[Any], class_labels: Optiona
     return cf_matrix
 
 class ClassificationReport:
+    """
+    A class to generate a classification report including precision, recall, F1-score, and support for each class,
+    as well as overall accuracy.
+    """
     def __init__(self, y_true: list[Any], y_pred: list[Any]) -> None:
+        """
+        Initializes the ClassificationReport with the true and predicted labels, computes the confusion matrix,
+        and calculates precision, recall, F1-score, and support for each class.
+
+        Args:
+            y_true (list[Any]): The true class labels.
+            y_pred (list[Any]): The predicted class labels.
+        """
         _check_length(y_true, y_pred)
         _check_type(y_true, y_pred)
 
@@ -59,7 +111,7 @@ class ClassificationReport:
         col_sum = cf_matrix.sum(axis=0)
         row_sum = cf_matrix.sum(axis=1)
 
-        self._diagonal_sum = 0
+        diagonal_sum = 0
 
         for idx, class_label in enumerate(class_labels):
             precision = cf_matrix[idx][idx] / col_sum[idx] if col_sum[idx] != 0 else 0
@@ -79,22 +131,41 @@ class ClassificationReport:
                 support
             ))
 
-            self._diagonal_sum += cf_matrix[idx][idx]
+            diagonal_sum += cf_matrix[idx][idx]
 
-        self._accuracy = self._diagonal_sum / sum(col_sum)
+        self._accuracy = diagonal_sum / sum(col_sum)
+        self._total_support = sum(row_sum)
 
     def _report_to_str(self) -> str:
+        """
+        Converts the classification report to a formatted string.
+
+        Returns:
+            str: A string representation of the classification report.
+        """
         header = "{: >10} {: >10} {: >10} {: >10} {: >10}".format("Class", "Precision", "Recall", "F1-Score", "Support")
         report = [header]
         for row in self._report_rows:
             report.append("{: >10} {: >10.2f} {: >10.2f} {: >10.2f} {: >10}".format(*row))
 
-        report.append("{: >10} {: >10.2f} {: >10}".format("Accuracy", self._accuracy, self._diagonal_sum))
+        report.append("{: >10} {: >10} {: >10} {: >10.2f} {: >10}".format("Accuracy", "", "", self._accuracy, self._total_support))
 
         return "\n".join(report)
 
     def __str__(self) -> str:
+        """
+        Returns the formatted classification report as a string.
+
+        Returns:
+            str: A string representation of the classification report.
+        """
         return self._report_to_str()
 
     def __repr__(self) -> str:
+        """
+        Returns the formatted classification report as a string.
+
+        Returns:
+            str: A string representation of the classification report.
+        """
         return self._report_to_str()
