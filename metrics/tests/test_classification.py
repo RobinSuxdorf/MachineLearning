bb134a48d54f560a4f9a798a72cdec1d84bdede7
Robin Suxdorf <robin.suxdorf@t-online.de>
2024-06-06 23:36:23 +0200

    Added test for classification report

--- metrics/tests/test_classification.py
+++ metrics/tests/test_classification.py
@@ -2,7 +2,8 @@ import numpy as np
 
 from metrics import (
     accuracy_score,
-    confusion_matrix
+    confusion_matrix,
+    ClassificationReport
 )
 
 def test_accuracy_sccore():
@@ -41,3 +42,32 @@ def test_confusion_matrix_multiclass_classification():
     ])
 
     assert np.array_equal(cm, expected_cm)
+
+def test_classification_report():
+    y_true = [0, 2, 1, 2, 1, 1, 0, 1]
+    y_pred = [2, 1, 0, 2, 2, 2, 0, 1]
+
+    report = ClassificationReport(y_true, y_pred)
+
+    expected_report = {
+        0: {
+            'precision': 0.5, 
+            'recall': 0.5, 
+            'f1_score': 0.5, 
+            'support': 2
+        },
+        1: {
+            'precision': 0.5,
+            'recall': 0.25,
+            'f1_score': 0.3333333333333333,
+            'support': 4
+        },
+        2: {
+            'precision': 0.25,
+            'recall': 0.5,
+            'f1_score': 0.3333333333333333,
+            'support': 2
+        }
+    }
+
+    report._report == expected_report
