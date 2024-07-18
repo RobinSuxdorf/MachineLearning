d7768e6fae1a480c7ffcd3683f571cf432b4c3d9
Robin Suxdorf <robin.suxdorf@t-online.de>
2024-07-18 19:34:48 +0200

    Added kmeans class

---
+++ mlalgos/cluster/kmeans.py
@@ -0,0 +1,9 @@
+class KMeans:
+    def __init__(self, n_clusters: int = 2) -> None:
+        self._n_clusters = n_clusters
+
+    def fit(self, X):
+        pass
+
+    def predict(self, X):
+        pass
