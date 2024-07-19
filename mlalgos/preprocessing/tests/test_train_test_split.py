b3c2021a8477883b0682605dc884d75a91e89e4c
Robin Suxdorf <robin.suxdorf@t-online.de>
2024-07-19 23:40:21 +0200

    Added implementation for train_test_split

--- mlalgos/preprocessing/tests/test_train_test_split.py
+++ mlalgos/preprocessing/tests/test_train_test_split.py
@@ -3,10 +3,13 @@ import re
 import numpy as np
 from mlalgos.preprocessing import train_test_split
 
+
 def test_train_test_split() -> None:
     X, y = np.arange(10).reshape((5, 2)), range(5)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42
+    )
 
     expected_X_train = np.array([[0, 1], [4, 5], [6, 7]])
     expected_y_train = np.array([0, 2, 3])
@@ -18,10 +21,14 @@ def test_train_test_split() -> None:
     assert np.array_equal(X_test, expected_X_test)
     assert np.array_equal(y_test, expected_y_test)
 
+
 def test_train_test_split_with_different_length() -> None:
     with pytest.raises(AssertionError, match=r"Length of X and y should match."):
         train_test_split(np.array([]), np.array([0]))
 
+
 def test_train_test_split_with_wrong_test_size() -> None:
-    with pytest.raises(AssertionError, match=re.escape("test_size should be in the interval [0,1].")):
+    with pytest.raises(
+        AssertionError, match=re.escape("test_size should be in the interval [0,1].")
+    ):
         train_test_split(np.array([]), np.array([]), test_size=-1)
