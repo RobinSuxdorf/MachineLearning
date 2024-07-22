543d32b07dd273b6cbd887840dff7c31b58f15cd
Robin Suxdorf <robin.suxdorf@t-online.de>
2024-07-23 00:39:32 +0200

    Added doc strings

--- mlalgos/cluster/affinity_propagation.py
+++ mlalgos/cluster/affinity_propagation.py
@@ -11,6 +11,16 @@ class AffinityPropagation:
         preferences: Optional[np.ndarray] = None,
         random_state: Optional[int] = None,
     ) -> None:
+        """
+        Initialization method for the Affinity Propagation clustering algorithm.
+
+        Args:
+            damping (float): Damping factor between 0.5 and 1.
+            max_iter (int): Maximum number of iterations.
+            convergence_iter (int): Number of iterations with no change in exemplars to declare convergence.
+            preferences (Optional[np.ndarray]): Preferences for each data point. If None, set to median similarity.
+            random_state (Optional[int]): Random seed for reproducibility.
+        """
         self._damping = damping
         self._max_iter = max_iter
         self._convergence_iter = convergence_iter
@@ -32,9 +42,25 @@ class AffinityPropagation:
         return self._labels
 
     def _negative_squared_distance(self, x: np.ndarray, y: np.ndarray) -> float:
+        """
+        Calculates the negative squared distance.
+
+        Args:
+            x (np.ndarray): A 1-dimensional array representing the first point.
+            y (np.ndarray): A 1-dimensional array representing the second point.
+
+        Returns:
+            float: The negative squared distance between x and y.
+        """
         return -np.power(np.linalg.norm(x - y), 2)
 
     def _calculate_similarity_matrix(self, X: np.ndarray) -> None:
+        """
+        Calculates the similarity matrix for the samples in X.
+
+        Args:
+            X (np.ndarray): The samples for which the similarity matrix will be calculated.
+        """
         self._sim_matrix = np.array(
             [[self._negative_squared_distance(x, y) for x in X] for y in X]
         )
@@ -42,6 +68,16 @@ class AffinityPropagation:
     def _calculate_responsibility_matrix(
         self, sim_matrix: np.ndarray, avail_matrix: np.ndarray
     ) -> np.ndarray:
+        """
+        Calculates the new responsibility matrix from the similarity matrix and the availability matrix.
+
+        Args:
+            sim_matrix (np.ndarray): The similarity matrix.
+            avail_matrix (np.ndarray): The previous availability matrix.
+
+        Returns:
+            np.ndarray: The new responsibility matrix.
+        """
         l = len(sim_matrix)
         new_resp_matrix = np.zeros((l, l))
 
@@ -55,6 +91,15 @@ class AffinityPropagation:
         return new_resp_matrix
 
     def _calculate_availability_matrix(self, resp_matrix: np.ndarray) -> np.ndarray:
+        """
+        Calculates the new availability matrix from the previous responsibility matrix.
+
+        Args:
+            resp_matrix (np.ndarray): The previous responsibility matrix.
+
+        Returns:
+            np.ndarray: The new availability matrix.
+        """
         l = len(resp_matrix)
         new_avail_matrix = np.zeros((l, l))
 
@@ -70,12 +115,24 @@ class AffinityPropagation:
                     )
                     new_avail_matrix[i][k] = min(0, resp_matrix[k][k] + sum_val)
                 else:
-                    sum_val = sum([max(0, resp_matrix[j][k]) for j in range(l) if j != k])
+                    sum_val = sum(
+                        [max(0, resp_matrix[j][k]) for j in range(l) if j != k]
+                    )
                     new_avail_matrix[k][k] = sum_val
 
         return new_avail_matrix
 
     def _assign_labels(self, X: np.ndarray, exemplars: np.ndarray) -> np.ndarray:
+        """
+        Assign the labels the samples belong to.
+
+        Args:
+            X (np.ndarray): The samples to be labeled.
+            exemplars (np.ndarray): The cluster centers.
+
+        Returns:
+            np.ndarray: An array containing the labels for the samples.
+        """
         exemplar_labels = {exemplar: label for label, exemplar in enumerate(exemplars)}
 
         labels = np.full(len(X), -1)
@@ -87,7 +144,16 @@ class AffinityPropagation:
                 nearest_exemplar = exemplars[np.argmax(self._sim_matrix[i, exemplars])]
                 labels[i] = exemplar_labels[nearest_exemplar]
 
+        return labels
+
     def fit(self, X: np.ndarray) -> None:
+        """
+        Compute the affinity propagation clustering.
+
+        Args:
+            X (np.ndarray): Training examples to cluster.
+
+        """
         self._calculate_similarity_matrix(X)
 
         if self._preferences is None:
@@ -132,6 +198,15 @@ class AffinityPropagation:
         self._labels = self._assign_labels(X, exemplars)
 
     def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict the closest exemplar each example in X belongs to.
+
+        Args:
+            X (np.ndarray): The data to predict.
+
+        Returns:
+            np.ndarray: The indices of the exemplar each sample belongs to.
+        """
         return np.array(
             [
                 np.argmax(
