ad6b1f53ce611fb009652f78d0c1a4069b1d3d13
Robin Suxdorf <robin.suxdorf@t-online.de>
2024-07-27 20:24:34 +0200

    Fixed test

--- mlalgos/metrics/tests/test_precision_recall_curve.py
+++ mlalgos/metrics/tests/test_precision_recall_curve.py
@@ -6,7 +6,7 @@ def test_precision_recall_curve() -> None:
     y_true = np.array([0, 0, 1, 1])
     y_scores = np.array([0.1, 0.4, 0.35, 0.8])
 
-    expected_precision = np.array([0.5, 0.66666667, 0.5, 1, 1])
+    expected_precision = np.array([0.5, 0.6666666666666666, 0.5, 1, 1])
     expected_recall = np.array([1, 1, 0.5, 0.5, 0])
     expected_thresholds = np.array([0.1 , 0.35, 0.4 , 0.8])
 
