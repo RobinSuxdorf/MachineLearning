7865bb4f26d61c9e397b151716455e9501376aee
Robin Suxdorf <robin.suxdorf@t-online.de>
2024-08-06 19:37:19 +0200

    Added inverse transform method

--- mlalgos/preprocessing/encoder.py
+++ mlalgos/preprocessing/encoder.py
@@ -1,24 +1,54 @@
-from typing import Any
+from typing import Any, Union
 import numpy as np
 
 
 class OneHotEncoder():
-    def __init__(self) -> None:
-        self._value_to_idx: dict[Any, str] = {}
+    def __init__(self, handle_unknown: str = "error") -> None:
+        self._handle_unknown = handle_unknown
+        self._categories: list[np.ndarray] = []
+        self._value_to_idx: dict[Any, int] = {}
+        self._idx_to_value: dict[int, Any] = {}
         self._dimension: int = 0
 
-    def fit(self, X: np.ndarray) -> None:
-        unique_values = np.unique(X)
-        self._value_to_idx = {value: idx for idx, value in enumerate(unique_values)}
-        self._dimension = len(self._value_to_idx)
+    def fit(self, X: Union[np.ndarray, list[Any]]) -> None:
+        if isinstance(X, list):
+            X = np.array(X, dtype=object)
+
+        if X.ndim == 1:
+            X = X.reshape(-1, 1)
+
+        self._categories = [np.unique(col) for col in zip(*X)]
+
+        idx = 0
+
+        for cat in self._categories:
+            for value in cat:
+                self._value_to_idx[value] = idx
+                self._idx_to_value[idx] = value
+
+                idx += 1
+        
+        self._dimension = idx
 
     def _row_to_one_hot(self, row: np.ndarray) -> np.ndarray:
         one_hot = np.zeros(self._dimension)
-        ids = [self._value_to_idx[x] for x in row]
+
+        ids: list[int] = []
+
+        for x in row:
+            try:
+                ids.append(self._value_to_idx[x])
+            except KeyError:
+                if self._handle_unknown == "error":
+                    raise KeyError(f"The value '{x}' is unknown.")
+
         one_hot[ids] = 1
         return one_hot
 
-    def transform(self, X: np.ndarray) -> np.ndarray:
+    def transform(self, X: Union[np.ndarray, list[Any]]) -> np.ndarray:
+        if isinstance(X, list):
+            X = np.array(X, dtype=object)
+
         if X.ndim == 1:
             X = X.reshape(-1, 1)
 
@@ -28,5 +58,29 @@ class OneHotEncoder():
         self.fit(X)
         return self.transform(X)
 
-    def inverse_transform(self, X: np.ndarray) -> np.ndarray:
-        pass
+    def _inverse_single_vector(self, one_hot: np.ndarray) -> np.ndarray:
+        if self._handle_unknown == "error" and np.sum(one_hot) != len(self._categories):
+            raise ValueError(f"The one hot encoded vector {one_hot} does not match the fitting data.")
+
+        data: list[Any] = []
+
+        for cat in self._categories:
+            found = False
+            for idx, x in enumerate(one_hot):
+                if x == 1 and self._idx_to_value[idx] in cat:
+                    data.append(self._idx_to_value[idx])
+                    found = True
+                    break
+            if not found:
+                data.append(None)
+
+        return np.array(data, dtype=object)
+
+    def inverse_transform(self, X: Union[np.ndarray, list[Any]]) -> np.ndarray:
+        if isinstance(X, list):
+            X = np.array(X, dtype=object)
+
+        if X.ndim == 1:
+            return self._inverse_single_vector(X)
+
+        return np.array([self._inverse_single_vector(x) for x in X], dtype=object)
--- mlalgos/preprocessing/tests/test_encoder.py
+++ mlalgos/preprocessing/tests/test_encoder.py
@@ -1,13 +1,15 @@
 import pytest
+from typing import Any
 import numpy as np
 
 from mlalgos.preprocessing import OneHotEncoder
 
 @pytest.mark.parametrize(
     "X, expected", [
-        (np.array([0, 1, 2, 1, 0]), np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]])),
-        (np.array(["bird", "cat", "dog", "cat", "bird"]), np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]])),
-        (np.array([['Male', 1], ['Female', 3], ['Female', 2]]), np.array([[1, 0, 0, 0, 1], [0, 0, 1, 1, 0], [0, 1, 0, 1, 0]]))
+        ([0, 1, 2, 1, 0], np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]], dtype=object)),
+        (np.array([0, 1, 2, 1, 0], dtype=object), np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]], dtype=object)),
+        (["bird", "cat", "dog", "cat", "bird"], np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]], dtype=object)),
+        (np.array([['Male', 1], ['Female', 3], ['Female', 2]], dtype=object), np.array([[0, 1, 1, 0, 0], [1, 0, 0, 0, 1], [1, 0, 0, 1, 0]], dtype=object))
     ]
 )
 def test_one_hot_encoder_fit_transform(X: np.ndarray, expected: np.ndarray) -> None:
@@ -17,8 +19,68 @@ def test_one_hot_encoder_fit_transform(X: np.ndarray, expected: np.ndarray) -> N
 
     assert np.array_equal(transformed, expected)
 
-# test inverse transform
+def test_one_hot_encoder_handle_unknown_error() -> None:
+    with pytest.raises(KeyError, match=r"The value '(\S+)' is unknown."):
+        encoder = OneHotEncoder("error")
 
-# test error handling
+        X = np.array([0, 1, 2, 1, 0])
 
-# test non np input
+        encoder.fit(X)
+
+        encoder.transform(np.array([0, 1, 2, 4]))
+
+def test_one_hot_encoder_handle_unknown_ignore() -> None:
+    encoder = OneHotEncoder("ignore")
+
+    X = np.array([0, 1, 2, 1, 0], dtype=object)
+
+    encoder.fit(X)
+
+    transformed = encoder.transform(np.array([0, 1, 2, 3], dtype=object))
+
+    expected = np.array([
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+        [0, 0, 0]
+    ])
+
+    assert np.array_equal(transformed, expected)
+
+@pytest.mark.parametrize(
+    "X, encoded_vector, expected", [
+        ([0, 1, 2, 1, 0], [0, 1, 0], np.array([1], dtype=object)),
+        ([['Male', 1], ['Female', 3], ['Female', 2]], [[1, 0, 0, 0, 1], [0, 1, 0, 1, 0]], np.array([['Female', 3],['Male', 2]], dtype=object))
+    ]
+)
+def test_one_hot_encoder_inverse_transform(X: np.ndarray, encoded_vector: np.ndarray, expected: np.ndarray) -> None:
+    encoder = OneHotEncoder()
+
+    encoder.fit(X)
+
+    inverse_transformed = encoder.inverse_transform(encoded_vector)
+
+    assert np.array_equal(inverse_transformed, expected)
+
+def test_one_hot_encoder_inverse_transform_handle_unknown_error() -> None:
+    with pytest.raises(ValueError, match=r"The one hot encoded vector \[0 0 0 1 0\] does not match the fitting data."):
+        encoder = OneHotEncoder("error")
+
+        X = np.array([['Male', 1], ['Female', 3], ['Female', 2]], dtype=object)
+
+        encoder.fit(X)
+
+        encoder.inverse_transform([[0, 1, 1, 0, 0], [0, 0, 0, 1, 0]])
+
+def test_one_hot_encoder_inverse_transform_handle_unknown_ignore() -> None:
+    encoder = OneHotEncoder("ignore")
+
+    X = np.array([['Male', 1], ['Female', 3], ['Female', 2]], dtype=object)
+
+    encoder.fit(X)
+
+    inverse_transformed = encoder.inverse_transform([[0, 1, 1, 0, 0], [0, 0, 0, 1, 0]])
+
+    expected = np.array([["Male", 1], [None, 2]], dtype=object)
+
+    assert np.array_equal(inverse_transformed, expected)
