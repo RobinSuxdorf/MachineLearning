42b2399d304ae20c7a5ef186db858820a537795d
Robin Suxdorf <robin.suxdorf@t-online.de>
2024-08-06 20:22:20 +0200

    Added tests

--- mlalgos/metrics/__init__.py
+++ mlalgos/metrics/__init__.py
@@ -1,2 +1,3 @@
 from .classification import accuracy_score, ClassificationReport, confusion_matrix
+from .levenshtein import levenshtein_distance
 from .precision_recall_curve import precision_recall_curve
--- mlalgos/metrics/levenshtein.py
+++ mlalgos/metrics/levenshtein.py
@@ -2,7 +2,7 @@ def levenshtein_distance(x: str, y: str) -> int:
     """
     Compute the Levenshtein distance between two strings using a recursive approach.
 
-    The Levenshtein distance (or edit distance) is a measure of the number of single-character edits 
+    The Levenshtein distance (or edit distance) is a measure of the number of single-character edits
     (insertions, deletions, or substitutions) required to transform one string into another.
 
     Args:
@@ -14,12 +14,12 @@ def levenshtein_distance(x: str, y: str) -> int:
     """
     if x == "" or y == "":
         return max(len(x), len(y))
-    
+
     if x[0] == y[0]:
         return levenshtein_distance(x[1:], y[1:])
 
     return 1 + min(
         levenshtein_distance(x[1:], y),
         levenshtein_distance(x, y[1:]),
-        levenshtein_distance(x[1:], y[1:])
+        levenshtein_distance(x[1:], y[1:]),
     )
