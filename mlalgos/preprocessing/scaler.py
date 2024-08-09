69fc4bcdf2bffeda8e861eca80f561044ad695d0
Robin Suxdorf <robin.suxdorf@t-online.de>
2024-08-09 16:47:02 +0200

    Implemented MinMaxScaler

--- mlalgos/preprocessing/scaler.py
+++ mlalgos/preprocessing/scaler.py
@@ -55,3 +55,66 @@ class StandardScaler:
             X = X + self._mean
 
         return X
+
+class MinMaxScaler:
+    def __init__(self, feature_range: tuple[float, float]) -> None:
+        if feature_range[0] >= feature_range[1]:
+            raise ValueError("The first value of feature_range must be smaller than the second value.")
+
+        self._min = feature_range[0]
+        self._max = feature_range[1]
+        self._data_min: Optional[np.ndarray] = None
+        self._data_max: Optional[np.ndarray] = None
+        self._scale: Optional[float] = None
+
+    @property
+    def min_(self) -> float:
+        return self._min
+
+    @property
+    def max_(self) -> float:
+        return self._data_max 
+
+    @property
+    def data_min_(self) -> float:
+        if self._data_min is None:
+            raise AttributeError("data_min_ is not set. You need to call 'fit' first.")
+        return self._data_min
+
+    @property
+    def data_max_(self) -> float:
+        if self._data_max is None:
+            raise AttributeError("data_max_ is not set. You need to call 'fit' first.")
+        return self._data_max
+
+    @property
+    def scale_(self) -> float:
+        if self._scale is None:
+            raise AttributeError("scale_ is not set. You need to call 'fit' first.")
+        return self._scale
+
+    def fit(self, X: Union[np.ndarray, list[Any]]) -> None:
+        if isinstance(X, list):
+            X = np.array(X)
+
+        self._data_min = X.min(axis=0)
+        self._data_max = X.max(axis=0)
+
+        self._scale = (self._max - self._min) / (self.data_max_ - self.data_min_)
+        return self
+
+    def transform(self, X: Union[np.ndarray, list[Any]]) -> np.ndarray:
+        if isinstance(X, list):
+            X = np.array(X)
+
+        return (X - self._data_min) * self._scale + self._min
+
+    def fit_transform(self, X: Union[np.ndarray, list[Any]]) -> np.ndarray:
+        self.fit(X)
+        return self.transform(X)
+
+    def inverse_transform(self, X: Union[np.ndarray, list[Any]]) -> np.ndarray:
+        if isinstance(X, list):
+            X = np.array(X)
+
+        return (X - self._min) / self._scale + self._data_min
