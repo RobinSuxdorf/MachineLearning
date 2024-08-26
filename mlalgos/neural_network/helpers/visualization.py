17952383eb110572ff7247ad6f15ef5ca161a74e
Robin Suxdorf <robin.suxdorf@t-online.de>
2024-08-26 11:26:46 +0200

    Added doc strings for visualization method

--- mlalgos/neural_network/helpers/visualization.py
+++ mlalgos/neural_network/helpers/visualization.py
@@ -2,6 +2,21 @@ from graphviz import Digraph
 from mlalgos.neural_network import Value
 
 def _trace(root: Value) -> tuple[set[Value], set[tuple[Value, Value]]]:
+    """
+    Trace the computational graph starting from the root Value node.
+
+    This function performs a depth-first traversal of the computational graph 
+    starting from the given root node and collects all nodes and edges involved 
+    in the computation. 
+
+    Args:
+        root (Value): The root node of the computational graph from which to start tracing.
+
+    Returns:
+        tuple[set[Value], set[tuple[Value, Value]]]: A tuple containing:
+            - A set of all Value nodes encountered in the computational graph.
+            - A set of edges represented as tuples of parent-child relationships between Value nodes.
+    """
     nodes: set[Value] = set()
     edges: set[tuple[Value, Value]] = set()
 
@@ -15,6 +30,19 @@ def _trace(root: Value) -> tuple[set[Value], set[tuple[Value, Value]]]:
     return nodes, edges
 
 def draw_computational_graph(root: Value) -> Digraph:
+    """
+    Draw the computational graph using Graphviz, starting from the root Value node.
+
+    This function visualizes the computational graph by creating a directed acyclic graph (DAG) 
+    using Graphviz. It represents each node's data and gradient and draws edges for operations 
+    performed between nodes.
+
+    Args:
+        root (Value): The root node of the computational graph to visualize.
+
+    Returns:
+        Digraph: A Graphviz Digraph object representing the computational graph.
+    """
     graph = Digraph(format="svg", graph_attr={"rankdir": "LR"})
 
     nodes, edges = _trace(root)
